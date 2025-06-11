ba21c778fc5ef22adbd0e8a9c072df8ef9bbe149
AQUELEBIXO\Bruno <leorecoa1@hotmail.com>
2025-06-11 02:42:07 -0300

    feat: atualizar configuração do Vercel, otimizar dependências e adicionar arquivo de configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
