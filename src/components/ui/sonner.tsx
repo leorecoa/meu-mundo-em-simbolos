940ffccff63ba208e60866d37cc69432b7e3d538
AQUELEBIXO\Bruno <leorecoa1@hotmail.com>
2025-06-11 11:43:43 -0300

    feat: simplificar script de build, remover dependência e ajustar tema do Toaster

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,10 +1,10 @@
-import { useTheme } from "next-themes"
 import { Toaster as Sonner, toast } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  // Usar um tema fixo em vez de next-themes
+  const theme = "light"
 
   return (
     <Sonner
