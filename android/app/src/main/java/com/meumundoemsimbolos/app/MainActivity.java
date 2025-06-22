884ab923434624997a30d503447d48b455975a2f
leorecoa <leorecoa2@gmail.com>
2025-06-22 17:12:37 -0300

    feat: adicionar implementação do método main na MainActivity e remover comentários desnecessários

--- android/app/src/main/java/com/meumundoemsimbolos/app/MainActivity.java
+++ android/app/src/main/java/com/meumundoemsimbolos/app/MainActivity.java
@@ -1,9 +1,7 @@
 package com.meumundoemsimbolos.app;
 
-/**
- * Atividade principal do aplicativo.
- * Esta é uma versão simplificada apenas para compilação.
- */
 public class MainActivity {
-    // Classe vazia para compilação
+    public static void main(String[] args) {
+        System.out.println("App iniciado");
+    }
 }
